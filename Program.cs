4390a931e21094955eb009a51b4e2b8fb12c6ecc
Thomas Dall Pedersen <tempo@mail.dk>
2024-02-27 12:58:40 +0100

    wip

--- Program.cs
+++ Program.cs
@@ -6,59 +6,76 @@ using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 using var context = new AppDbContext();
 
-var flightId = 1;
-var aircraftId = 1;
 
 
 
-// Object vs foreign key
 
-//Flight flight = context.Flights.Find(flightId);
-//Console.WriteLine("AircraftId: " + flight.AircraftId);
+// Object vs foreign key reference
 
-//Aircraft aircraft = context.Aircrafts.Find(aircraftId);
+
+// Object reference
+// Her er 3 kald til Db
+
+//Flight flight = context.Flights.Find(1);
+//Aircraft aircraft = context.Aircrafts.Find(1);
 //flight.Aircraft = aircraft;
-//Console.WriteLine("AircraftId: " + flight.Aircraft.AircraftId);
+//context.SaveChanges();
 
 
+// Foreign key reference
+// Her er 2 kald til Db
 
-//Flight flight = context.Flights.Find(flightId);
-//Console.WriteLine("AircraftId: " + flight.AircraftId);
-
-//flight.AircraftId = aircraftId;
-//Console.WriteLine("AircraftId: " + flight.AircraftId);
-
-
-//var count = context.SaveChanges();
-//Console.WriteLine("AircraftId: " + flight.AircraftId);
-
-//Console.WriteLine(count);
-
+//Flight flight = context.Flights.Find(1);
+//flight.AircraftId = 1;
+//context.SaveChanges();
 
 
 
 // Bulk operationer
 
+
+// Single delete
+// Her bliver de 2000 ændringer gemt hver gang.
 //var sw = new Stopwatch();
 //sw.Start();
+//int total = 0;
+//var flights = context.Flights.Where(x => x.FlightId > 0).ToList();
+//foreach (var flight in flights)
+//{
+//    context.Flights.Remove(flight);
+//    var count = context.SaveChanges();
+//    total += count;
+//}
+//sw.Stop();
+//Console.WriteLine("Number of DELETE statements: " + total);
+//Console.WriteLine("Duration: " + sw.ElapsedMilliseconds);
 
+
+// Batch delete
+// Her bliver de 2000 ændringer gemt en enkelt gang til sidst.
+
+//var sw = new Stopwatch();
+//sw.Start();
 //int total = 0;
 //var flights = context.Flights.Where(x => x.FlightId > 0).ToList();
 
 //foreach (var flight in flights)
 //{
 //    context.Flights.Remove(flight);
-//    //var count = context.SaveChanges();
-//    //total += count;
 //}
 //total = context.SaveChanges();
 //sw.Stop();
 //Console.WriteLine("Number of DELETE statements: " + total);
 //Console.WriteLine("Duration: " + sw.ElapsedMilliseconds);
 
-//int total = 0;
+
+// Pseudo object
+// Her loader vi ikke de 2000 objekter fra Db, men laver 2000 nye og
+// sætter et Remove parameter på.
+
 //var sw = new Stopwatch();
 //sw.Start();
+//int total = 0;
 //for (int i = 0; i <= 2000; i++)
 //{
 //    var f = new Flight() { FlightId = i };
@@ -76,11 +93,7 @@ var aircraftId = 1;
 
 //var sw = new Stopwatch();
 //sw.Start();
-
-
 //var total = context.Database.ExecuteSqlRaw("DELETE FROM Flights WHERE FlightId > 0");
-
-
 //sw.Stop();
 //Console.WriteLine(total);
 //Console.WriteLine("Duration: " + sw.ElapsedMilliseconds);
